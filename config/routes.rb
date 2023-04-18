073b3d7eea27f1b004b79cb51e78516be8b28b7a
Syunsuke Komma <syunsuke@weseek.co.jp>
2023-04-18 04:45:01 +0000

    run "rails generate administrate:install"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :tasks
+      resources :swimlanes
+      resources :sprints
+
+      root to: "sprints#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
