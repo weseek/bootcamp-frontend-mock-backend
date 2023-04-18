33ad8705c0461e3604c9724b61f13b8ad7d84f9a
Syunsuke Komma <syunsuke@weseek.co.jp>
2023-04-18 04:48:45 +0000

    add validation in Task#status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :swimlane
   belongs_to :assignee, class_name: 'User', optional: true
+
+  validates :status, inclusion: { in: ['new', 'in progress', 'resolved', 'feedback', 'rejected'] }
 end
