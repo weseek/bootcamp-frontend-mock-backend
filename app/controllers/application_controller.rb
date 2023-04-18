3322a063345536e7cb223a97212b6d81b37c677e
Syunsuke Komma <syunsuke@weseek.co.jp>
2023-04-18 06:27:47 +0000

    set request/response key format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  before_action :underscore_root_keys
+
+  def underscore_root_keys
+    params.transform_keys!(&:underscore)
+  end
 end
