d67bff463c0b2ce1abaae3121a9beb19b7ca2a75
Syunsuke Komma <syunsuke@weseek.co.jp>
2023-05-25 03:19:19 +0000

    add administrate in asset pipeline manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+// for administrate
+//= link administrate/application.css
+//= link administrate/application.js
